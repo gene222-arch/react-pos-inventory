24adf31bfb911d0f65490be76526810d02b10870
unknown <genephillip222@gmail.com>
2021-02-15 23:39:37 +0800

    chore: implement customers module services

---
+++ src/services/customers/customers.js
@@ -0,0 +1,75 @@
+import axiosInstance from '../../utils/axiosInstance'
+import * as Helpers from '../../utils/helpers'
+
+
+export const fetchAllAsync = async () => 
+{
+    try {
+        
+        const result = await axiosInstance().get('/customers');
+
+        return result.data;
+
+    } catch (error) {
+        return error.response.data;        
+    }
+}
+
+
+export const fetchAsync = async (payload) => 
+{
+    try {
+        
+        const result = await axiosInstance()
+            .post('/customers/details', Helpers.prepareToFormData(payload));
+
+        return result.data;
+
+    } catch (error) {
+        return error.response.data;        
+    }
+}
+
+
+export const storeAsync = async (payload) => 
+{
+    try {
+        
+        const result = await axiosInstance().post('/customers', Helpers.prepareToFormData(payload));
+
+        return result.data;
+
+    } catch (error) {
+        return error.response.data;        
+    }
+}
+
+
+export const updateAsync = async (payload) => 
+{
+    try {
+        
+        const result = await axiosInstance()
+            .post('/customers', Helpers.prepareToFormData(payload, 'PUT'));
+
+        return result.data;
+
+    } catch (error) {
+        return error.response.data;        
+    }
+}
+
+
+export const destroyAsync = async (payload) => 
+{
+    try {
+        
+        const result = await axiosInstance()
+            .delete('/customers', Helpers.prepareToFormData(payload));
+
+        return result.data;
+
+    } catch (error) {
+        return error.response.data;        
+    }
+}
